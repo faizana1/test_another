8eac3dad51bc4cc8cb6b1c70b64eddf053071b54
abeer <abeer@DESKTOP-SBB4154>
2018-07-07 13:32:40 -0400

    Add project files.

---
+++ test_another/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace test_another.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
